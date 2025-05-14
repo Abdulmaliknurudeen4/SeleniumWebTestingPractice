0c2c8ab92e3b1e7ac97dde45daa40256a8d8dc8a
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2025-05-14 14:06:21 +0100

    Using Hasmap

--- src/test/java/org/example/submitOrderSample.java
+++ src/test/java/org/example/submitOrderSample.java
@@ -7,22 +7,23 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import static org.testng.Assert.assertTrue;
 
 public class submitOrderSample extends BaseTest {
 
     @Test(dataProvider = "getData", groups = {"Purchase"})
-    public  void orderTest(String email, String password, String productName) throws IOException {
+    public void orderTest(HashMap<String, String> input) throws IOException {
 
 
         ProductCatalogue productCatalogue = launchApplication()
-                .loginApplication(email, password);
-        productCatalogue.addProductToCart(productName);
+                .loginApplication(input.get("email"), input.get("password"));
+        productCatalogue.addProductToCart(input.get("product"));
 
 
         CartPage cartPage = productCatalogue.gotoCart();
-        boolean findProduct = cartPage.findProduct(productName);
+        boolean findProduct = cartPage.findProduct(input.get("product"));
         assertTrue(findProduct);
 
 
@@ -36,7 +37,7 @@ public class submitOrderSample extends BaseTest {
         Assert.assertTrue(confirmationText.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
     }
 
-    @Test(dataProvider = "getData",dependsOnMethods = {"orderTest"})
+    @Test(dataProvider = "getData", dependsOnMethods = {"orderTest"})
     public void orderHistoryTest(String email, String password, String productName) throws IOException {
         ProductCatalogue productCatalogue = launchApplication()
                 .loginApplication(email, password);
@@ -46,11 +47,20 @@ public class submitOrderSample extends BaseTest {
     }
 
     @DataProvider
-    public Object[][] getData(){
+    public Object[][] getData() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("email", "abdulmaliknurudeen4@gmail.com");
+        map.put("password", "Password360444#");
+        map.put("product", "IPHONE 13 PRO");
+
+        HashMap<String, String> map2 = new HashMap<>();
+        map2.put("email", "abdulmaliknurudeen5@gmail.com");
+        map2.put("password", "Jadam73737##");
+        map2.put("product", "ADIDAS ORIGINAL");
         //user name, password, and product set set
-       return new Object[][] {
-               {"abdulmaliknurudeen4@gmail.com", "Password360444#", "IPHONE 13 PRO"},
-               {"abdulmaliknurudeen5@gmail.com","Jadam73737##", "ADIDAS ORIGINAL"}
-       };
+        return new Object[][]{
+                {map},
+                {map2}
+        };
     }
 }
