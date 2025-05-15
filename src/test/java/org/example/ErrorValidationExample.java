f5be969ef1b77bf1dd7827fd6f072b9b63bba71a
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2025-05-15 14:24:23 +0100

    Retry Mechanism

--- src/test/java/org/example/ErrorValidationExample.java
+++ src/test/java/org/example/ErrorValidationExample.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.pageObjects.ProductCatalogue;
 import org.example.testComponents.BaseTest;
+import org.example.testComponents.Retry;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,7 +10,7 @@ import java.io.IOException;
 
 public class ErrorValidationExample extends BaseTest {
 
-    @Test(groups = {"errorHandling"})
+    @Test(groups = {"errorHandling"}, retryAnalyzer = Retry.class)
     public  void main() throws IOException {
         String username = "abdulmalrudeen4@gmail.com";
         String password = "Password360444#";
